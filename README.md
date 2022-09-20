3c6c69477c8d90c9a04ceb7470816f447564c78a
Ted Hilger <tedhilger@gmail.com>
2022-09-19 21:15:06 -0400

    Adding the completed project edits

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# cs50-final-project
+# Pottery Store Flask Application
+#### Video Demo: https://www.youtube.com/watch?v=HiLwbyjL4SY&ab_channel=TedHilger
+
+### Description
+
+#### Introduction
+Pottery Store Flask Application allows users to shop for pottery and studio merchandise. Users can view information about the pottery studio, register/log-in, add items to their shopping cart, checkout, and view their order history. There is also seperate functionality for an adminstrative user with certain log-in credentials, which enables the admin to add or remove items to the store and view information on users' orders. 
+
+#### How It's Made
+This project was made using Flask (Python, HTML5/CSS , and SQL). A log-in and session module enable much of the functionality, including shopping cart, order history, and admin control. 
+
+#### Usage
+The About page shows studio information for Togei Kyoshitsu, the pottery studio for which this application was created. Upon clicking the shop option along the navigation bar, the user is redirected to the store where they have the option to shop for pottery or additional studio merchandise. I believe that seperating these two categories better organizes the store for users. I had to learn how to dynamically pull product images from a static folder to display them on the store in a way that would allow new images to be added through the admin UX. This is done through a SQL table that contains the image names that correspond with the images stored in the project's static folder. Through this strategy, maniupulation of the store was made much easier because manipulating the SQL tables is simple. 
+By clicking 'View', the user can see additional information on the items including a description that is pulled from a SQL product table. Adding to cart stores the information in the session variable. Checking out stores the items from the user's cart in a SQL purchased table using a for-loop for each item within the cart session. Checking out also removes the items from the store in order to prevent multiple purchases of the same item, and is reflected within the SQL product table. I had to strategize how best to store all of the relevant information, and ultimately decided that multiple tables with different parts of the complete order information was most efficient and simple to manipulate. Specifcially, there is a basic table that creates the order number, one with the user's shipping and payment info, and one with additional details on the purchased products. These all correspond with each other by using the order number. 
+On the admin side, showing a different view is possible through using the session variable to verify that the username is proper. Jinja if-statements allow different views for similar webpages. The admin is able to re-add items that were removed from the store from user-orders. The admin can also view information on the orders that were made by users, and add new items to the store. Upon adding the image and necessary information for the new item, a new SQL entry is added to the products table, and the uploaded image is added to the project's static image folder. 
+
+
+
+
--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template, flash, request, redirect, session
+from flask import Flask, render_template, flash, request, redirect, session, url_for
 from flask_session import Session
 import os, re, sqlite3
+from datetime import date
 from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 from helpers import login_required, usd
 
+UPLOAD_FOLDER = 'static/images/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
 
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
 Session(app)
+
 
 app.jinja_env.filters["usd"] = usd
 db = SQL("sqlite:///inventory.db")
@@ -20,35 +28,59 @@ db = SQL("sqlite:///inventory.db")
 @app.route('/', methods=["GET", "POST"])
 def index():
 
-    images = db.execute("SELECT * FROM products WHERE isMerch = 0")
+    images = db.execute("SELECT * FROM products WHERE isMerch = 0 AND inStock = 1 ORDER BY productID DESC")
     session["item"] = None
 
     if request.method == "POST":
-        session["item"] = request.form.get("id")
-        return redirect('/item')
-    
+        if request.form.get("id") == 'admin':
+            return redirect('/newfile')
+        if id:
+            session["item"] = request.form.get("id")
+            return redirect('/item')
+
     return render_template("index.html", images=images, usd=usd)
 
 @app.route('/merch', methods=['GET', 'POST'])
 def merch():
 
-    images = db.execute("SELECT * FROM products WHERE isMerch = 1")
+    images = db.execute("SELECT * FROM products WHERE isMerch = 1 AND inStock = 1 ORDER BY productID DESC")
     session["item"] = None
         
     if request.method == "POST":
-        session["item"] = request.form.get("id")
-        return redirect('/item')
+        if request.form.get("id") == 'admin':
+            return redirect('/newfile')
+        if id:
+            session["item"] = request.form.get("id")
+            return redirect('/item')
 
     return render_template("merch.html", images=images, usd=usd)
 
-@app.route('/item', methods=['GET', "POST"])
-def item():
-    if 'cart' not in session:
-        session['cart'] = []
+@app.route('/item', methods=['GET'])
+def get_item():
 
     if request.method == "GET":
+        carted = False
+        if not 'cart' in session:
+            session['cart'] = []
         item = db.execute("SELECT * FROM products WHERE productID = ?", session["item"])
-        return render_template("item.html", item=item, usd=usd)
+        
+        if session['cart'] != None:
+            if session['item'] in session['cart']:
+                carted = True
+
+    return render_template("item.html", item=item, carted=carted, usd=usd)
+
+@app.route('/item', methods=['POST'])
+@login_required
+def post_item():
+    if not 'cart' in session or (session['cart'] == None):
+        session['cart'] = []
+    if session['user_id'] == "admin":
+        id = request.form.get("id")
+        if id:
+            db.execute("UPDATE products SET inStock = 0 WHERE productID = ?", id)
+            flash("Item removed from store")
+            return redirect("/")
 
     if request.method == "POST":
         id = request.form.get("id")
@@ -62,9 +94,9 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 @app.route('/cart', methods=["GET", "POST"])
+@login_required
 def cart():
-
-    if 'cart' not in session:
+    if not 'cart' in session:
         session['cart'] = []
 
     if request.method == "POST":
@@ -73,39 +105,66 @@ def cart():
             session['cart'].remove(id)
         flash("Removed from cart")
         return redirect("/cart")
-
-    items = db.execute("SELECT * FROM products WHERE productID IN (?)", session['cart'])
-
-    if len(session['cart']) == 0:
+            
+    if session['cart'] == []:
         empty = True
     else:
         empty = False
+    
+    if not empty:
+        print(session['cart'])
+        items = db.execute("SELECT * FROM products WHERE productID IN (?)", session['cart'])
+        total = sum([x['price'] for x in items])
+        session['total'] = total
+    else:
+        items = []
+        total = 0
+        session['total'] = total
 
-    return render_template("cart.html", items=items, usd=usd, empty=empty)
+    return render_template("cart.html", items=items, usd=usd, empty=empty, total=total)
 
 @app.route("/checkout", methods=["GET", "POST"])
+@login_required
 def checkout():
+    
+    if request.method == "POST":
+        email = request.form.get('email')
+        user_ID = session['user_id']
+        first = request.form.get("first")
+        last = request.form.get("last")
+        address = request.form.get("address")
+        city = request.form.get("city")
+        state = request.form.get("state")
+        zip = int(request.form.get("zip"))
+            #adding order
+        if not re.search('@', email):
+            flash('Email must follow proper format, i.e. "@example.com"')
+            return redirect("/checkout")
+        try:
+            db.execute("INSERT INTO orders (userID, date, amount, placed) VALUES (?, ?, ?, ?)", user_ID, date.today(), session['total'], 0)
+                    #adding shipping to order
+            db.execute("INSERT INTO shipping (userID, first, last, address, city, state, zip) VALUES (?, ?, ?, ?, ?, ?, ?)", user_ID, first, last, address, city, state, zip)
+                    #adding bought items - need to get orderID that was just created
+            order_ID = db.execute("SELECT MAX(id) AS id FROM orders WHERE userID = ?", user_ID)
+            order_ID = order_ID[0]['id']
+            for item in session['cart']:
+                db.execute("INSERT INTO bought (productID, orderID, userID) VALUES (?, ?, ?)", item, order_ID, user_ID)
+                db.execute("UPDATE products SET inStock = 0 WHERE productID = ?", item)
+                session['cart'] = []
 
-            try:
-            #for product in session['cart']:
-                #db.execute("UPDATE products SET inStock = 0")
-                #db.execute("INSERT into ORDERS productID, userID, date")
-                db.execute("UPDATE products,  (email, password) VALUES (?, ?)", email, hash)
-                flash("Thank you for your order!")
-                session["cart"] = None
-                return redirect("/cart")
-            
-            except:
-                flash("Email already exists!")
-                return redirect("/register")
+            flash("Successfully ordered, thank you!")
+            return redirect("/cart")
+
+        except:
+            flash("Sorry, could not place order at this time")
+            return redirect("/cart")
+
     return render_template("checkout.html")
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
     if request.method == "GET":
 
         return render_template("login.html")
-
     # Forget any user_id
     session.clear()
 
@@ -129,24 +188,21 @@ def login():
         if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
             flash("Invalid email and/or password")
             return redirect("/login")
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["userID"]
-        session["name"] = rows[0]["email"]
-
-        # Redirect user to home page
+        
+        if request.form.get('email') == 'admin':
+            session["user_id"] = "admin"
+        else:
+            session["user_id"] = rows[0]["id"]
         return redirect("/profile")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return redirect("/login")
 
 @app.route("/about")
 def about():
+
     return render_template("about.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+
     email = request.form.get("email")
     password = request.form.get("password")
     confirmation = request.form.get("confirmation")
@@ -197,14 +253,88 @@ def logout():
     # Redirect user to login form
     return redirect("/login")
 
-@app.route('/profile')
+@app.route('/profile', methods=["GET", "POST"])
+@login_required
 def profile():
-    return render_template("profile.html")
+    admin = False
+    if session["user_id"] == "admin":
+        admin = True
+    if request.method == 'POST':
+        item = request.form.get("id")
+        db.execute("UPDATE products SET inStock = 1 WHERE productID = ?", item)
+        flash("Re-added to store")
+        return redirect("/profile")
+    #need to join product info from products ordered with order info to give relevant info - repeat for admin side orders
+    # add "filled" BIT section to orders to easily change whether orders appear as filled or not
+    products = db.execute("SELECT DISTINCT products.name, orders.date, products.price FROM orders JOIN bought ON orders.userID = bought.userID JOIN products ON products.productID = bought.productID WHERE orders.userID = ?", session['user_id'])
+    outStock = db.execute("SELECT * FROM products WHERE inStock = 0 ORDER BY productID DESC")
+    print(products)
+    return render_template("profile.html", products=products, usd=usd, admin=admin, outStock=outStock)
 
-@app.route('/orders')
+@app.route('/orders', methods=["GET", "POST"])
+@login_required
 def orders():
-    return render_template("orders.html")
+    if request.method == 'GET':
+        unfilled = db.execute("SELECT id, date FROM orders WHERE placed = 0")
+        filled = db.execute("SELECT id, date FROM orders WHERE placed = 1")
+        return render_template("orders.html", filled=filled, unfilled=unfilled, usd=usd)
+    if request.method == 'POST':
+        id = request.form.get('id')
+        placed = db.execute("SELECT placed FROM orders WHERE id = ?", id)
+        placed = placed[0]['placed']
+        print(placed)
+        if placed == 0:
+            placed = 1
+        else:
+            placed = 0
+        db.execute("UPDATE orders SET placed = ? WHERE id = ?", placed, id)
+        flash('Updated order status')
+        return redirect('/orders')
 
-@app.route('/admin')
-def admin():
-    return render_template("admin.html")
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/newitem', methods=['GET', 'POST'])
+@login_required
+def newitem():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        price = request.form.get("price")
+        description = request.form.get("description")
+        type = request.form.get('type')
+        if type == 'merch':
+            type = 1
+        else:
+            type = 0
+        if 'file' not in request.files:
+            flash('No file')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            db.execute("INSERT INTO products (name, price, description, inStock, isMerch, image) VALUES (?, ?, ?, ?, ?, ?)", name, price, description, 1, type, filename)
+            return redirect("/")
+    return render_template('newitem.html')
+
+@app.route('/info', methods=['GET', 'POST'])
+@login_required
+def info():
+    if request.method == "POST":
+        session['order'] = request.form.get('order')
+        return redirect('/info')
+    if request.method == "GET":
+        order = session['order']
+        print(order)
+        shipping = db.execute("SELECT * FROM shipping WHERE id = ?", order)
+        print(shipping)
+        total = db.execute("SELECT amount FROM orders WHERE id = ?", order)
+        print(total)
+        products = db.execute("SELECT products.name FROM products JOIN bought ON products.productID = bought.productID WHERE bought.orderID = ?", order)
+        print(products)
+        return render_template('info.html', products=products, shipping=shipping, total=total, order=order, usd=usd)
